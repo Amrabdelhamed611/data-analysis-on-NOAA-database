2204a857d22691735cff1e184726e2c0414a34ef
Amrabdelhamed611 <amr.abdelhamed1996@gmail.com>
2020-04-11 17:43:25 +0200

    enhance plots

--- RmarkdownRm.md
+++ RmarkdownRm.md
@@ -74,21 +74,22 @@ levels(NOAASub$PROPDMGEXP)
 ##  [1] ""  "-" "?" "+" "0" "1" "2" "3" "4" "5" "6" "7" "8" "B" "h" "H" "K" "m" "M"
 ```
 Now create varible by assigning values of symbols as same order of union output each symbol has value.   
-"" ,"-" ,"?" ,"+" = 0 ,and the numeric's = 1 for "" tricky symbol will convert it to char later.    
+"" ,"-" ,"?" ,"+" = 0 ,and the numeric's 10 to the power of the value for "" tricky symbol will convert it to char later.    
 "K" for thousands,"M" for millions,"B" for billions, and "H" for hundreds ,then will name the varible elements as same order of union to map btween symbols and values.
 
 ```r
 NOAASub$PROPDMGEXP <- as.factor(toupper(NOAASub$PROPDMGEXP))
 NOAASub$CROPDMGEXP <- as.factor(toupper(NOAASub$CROPDMGEXP))
 NOAASub$EVTYPE <- as.factor(toupper(NOAASub$EVTYPE))
-decode <- c(rep(0,4),rep(1,9),10**12,100,10**3,10**6)
+decode <- c(rep(0,4),1,10,10**2,10**3,10**4,10**5,10**6,10**7,10**8,10**12
+            ,100,10**3,10**6)
 names(decode) <- as.factor(union(levels(NOAASub$PROPDMGEXP),levels(NOAASub$CROPDMGEXP)))
 decode
 ```
 
 ```
 ##           -     ?     +     0     1     2     3     4     5     6     7     8 
-## 0e+00 0e+00 0e+00 0e+00 1e+00 1e+00 1e+00 1e+00 1e+00 1e+00 1e+00 1e+00 1e+00 
+## 0e+00 0e+00 0e+00 0e+00 1e+00 1e+01 1e+02 1e+03 1e+04 1e+05 1e+06 1e+07 1e+08 
 ##     B     H     K     M 
 ## 1e+12 1e+02 1e+03 1e+06
 ```
@@ -142,6 +143,7 @@ Tthen sum the FATALITIES and INJURIES, to get which types of events harmful with
 And, also sum the crop damge and properties damge, to get which types of events have the greatest economic consequences 
 
 ```r
+NOAASub$EVTYPE <- as.factor(tolower(NOAASub$EVTYPE))
 NOAASub <- aggregate(.~EVTYPE, data =NOAASub, sum)
 NOAASub$HarmfulHealth <-rowSums(NOAASub[,c("FATALITIES","INJURIES")])
 NOAASub$HarmfulEconomic <-rowSums(NOAASub[,c("CROPDMG","PROPDMG")])
@@ -187,42 +189,73 @@ paste("Total crops damage cost around",round(CropDmg/10**12,2)," billion US doll
 ```
      
 ### Events harmful with respect to population health.
+Plot the top 10 event responsible for Fatalities and which has maximum Fatalities.
 
 ```r
-TopHealthHarm <- arrange(NOAASub,HarmfulHealth) %>%
-                 subset(.,select = c(EVTYPE,HarmfulHealth))%>% `rownames<-`(.,NULL) %>%
+TopFATALITIES <- subset(NOAASub,select = c(EVTYPE,FATALITIES))%>% 
                  top_n(.,10) 
 ```
 
 ```
-## Selecting by HarmfulHealth
+## Selecting by FATALITIES
 ```
 
 ```r
-g<-ggplot(TopHealthHarm , aes(x=EVTYPE, y= HarmfulHealth/10**3)) 
+g<-ggplot(TopFATALITIES, aes(x=reorder(EVTYPE,FATALITIES), y= FATALITIES/10**3)) 
 (g + geom_bar(stat="identity",width=0.5, color="black", fill="red4")
    + coord_flip()
-   + labs(title = "Events most harmful with respect to population health") 
-   + labs(x = "Event Type", y = "FATALITIES and INJURIES (by thousands)")
-   + theme_minimal()    )
+   + labs(title = "Harmful Events with respect to Fatalities") 
+   + labs(x = "Event", y = "Fatalities (by thousands)")
+   + theme_minimal()  )
 ```
 
 ![](RmarkdownRm_files/figure-html/unnamed-chunk-14-1.png)<!-- -->
 
 ```r
-toph <- TopHealthHarm[which.max(TopHealthHarm$HarmfulHealth),]
-paste(toph[[1]],"'s are the most harmful with respect to population health on ",round(toph[2]/10**3,4)," thousands citizen",sep = "")
+toph <- TopFATALITIES[which.max(TopFATALITIES$FATALITIES),]
+paste("The Most FATALITIES caused by",toph[[1]],"near",round(toph[2]/10**3,4),"thousand citizen")
 ```
 
 ```
-## [1] "TORNADO's are the most harmful with respect to population health on 96.979 thousands citizen"
+## [1] "The Most FATALITIES caused by tornado near 5.633 thousand citizen"
+```
+
+Plot the top 10 event responsible for Injuries and which has maximum Injuries.
+
+```r
+TopINJURIES <- subset(NOAASub,select = c(EVTYPE,INJURIES))  %>% 
+               top_n(.,10) 
+```
+
+```
+## Selecting by INJURIES
+```
+
+```r
+g<-ggplot(TopINJURIES , aes(x=reorder(EVTYPE,INJURIES), y= INJURIES/10**3)) 
+(g + geom_bar(stat="identity",width=0.5, color="black", fill="firebrick3")
+    + coord_flip()
+    + labs(title = "Harmful Events with respect to Injuries") 
+    + labs(x = "Event", y = " Injuries (by thousands)")
+    + theme_minimal()  )
+```
+
+![](RmarkdownRm_files/figure-html/unnamed-chunk-15-1.png)<!-- -->
+
+```r
+toph <- TopINJURIES[which.max(TopINJURIES$INJURIES),]
+paste("The Most INJURIES caused by",toph[[1]],"near",round(toph[2]/10**3,4),"thousand citizen")
+```
+
+```
+## [1] "The Most INJURIES caused by tornado near 91.346 thousand citizen"
 ```
             
 ### Types of events have the greatest economic consequences
+Plot the top 10 event responsible for economic damage and which has maximum economic damage.
 
 ```r
-TopEconHarm <- arrange(NOAASub,HarmfulEconomic) %>%
-               subset(.,select = c(EVTYPE,HarmfulEconomic)) %>%
+TopEconHarm <- subset(NOAASub,select = c(EVTYPE,HarmfulEconomic))%>% 
                top_n(.,10)
 ```
 
@@ -231,22 +264,23 @@ TopEconHarm <- arrange(NOAASub,HarmfulEconomic) %>%
 ```
 
 ```r
-p<-ggplot(TopEconHarm , aes(x=as.factor(EVTYPE), y= (HarmfulEconomic/10**12))) 
+p<-ggplot(TopEconHarm , aes(x=reorder(EVTYPE,HarmfulEconomic), y= (HarmfulEconomic/10**12))) 
 (p + geom_bar(stat="identity",width=0.5,color="black", fill="blue3")
    + coord_flip()
-   + labs(title = "Events have the greatest economic consequences ") 
-   + labs(x = "Event Type", y = "economic cost by billions US dollar")
+   + labs(title = "Events have the greatest economic Cost ") 
+   + labs(x = "Event", y = "Cost by billion US dollar")
    + theme_minimal()  )
 ```
 
-![](RmarkdownRm_files/figure-html/unnamed-chunk-15-1.png)<!-- -->
+![](RmarkdownRm_files/figure-html/unnamed-chunk-16-1.png)<!-- -->
 
 ```r
 toph <- TopEconHarm[which.max(TopEconHarm$HarmfulEconomic),]
-paste(toph[[1]],"'s has the most greatest economic consequences near to  ",round(toph[2]/10**12,4)," billion US dollar",sep = "")
+paste("The Most economic damage caused by" , toph[[1]] ,"around" ,round(toph[2]/10**12,4) ,"billion US Dollar")
 ```
 
 ```
-## [1] "FLOOD's has the most greatest economic consequences near to  122.5278 billion US dollar"
+## [1] "The Most economic damage caused by flood around 122.5278 billion US Dollar"
 ```
+
 
--- analysis_scrip.R
+++ analysis_scrip.R
@@ -35,12 +35,12 @@ union(levels(NOAASub$PROPDMGEXP),levels(NOAASub$CROPDMGEXP))
 ##-----------------------------------------------------------------------
 ##-----------------------------------------------------------------------
 #will assign values accourding to union output as will assign it to names
-#"" ,"-" ,"?" ,"+" = 0 ,and the numeric's = 1
+#"" ,"-" ,"?" ,"+" = 0 ,and the numeric's valus is power
 #"K" for thousands,"M" for millions,"B" for billions, and "H" for hundreds
 
-decode <- c(rep(0,4),rep(1,9),10**12,100,10**3,10**6)
+decode <- c(rep(0,4),1,10,10**2,10**3,10**4,10**5,10**6,10**7,10**8,10**12,100,10**3,10**6)
 names(decode) <- as.factor(union(levels(NOAASub$PROPDMGEXP),levels(NOAASub$CROPDMGEXP)))
-names(decode)
+decode
 
 NOAASub <- aggregate(.~EVTYPE + CROPDMGEXP +PROPDMGEXP, data = NOAASub, sum)
 
@@ -62,39 +62,110 @@ NOAASub$PROPDMGEXP<- sapply(NOAASub$PROPDMGEXP, xpdecoder)
 NOAASub$CROPDMG <-as.numeric( NOAASub$CROPDMGEXP * NOAASub$CROPDMG)
 NOAASub$PROPDMG <-as.numeric( NOAASub$PROPDMGEXP * NOAASub$PROPDMG)
 NOAASub <- subset(NOAASub,select = -c(CROPDMGEXP,PROPDMGEXP) )
+NOAASub$EVTYPE <- as.factor(tolower(NOAASub$EVTYPE))
 NOAASub <- aggregate(.~EVTYPE, data =NOAASub, sum)
 ##-----------------------------------------------------------------------
 ##-----------------------------------------------------------------------
-NOAASub$HarmfulHealth <-rowSums(NOAASub[,c("FATALITIES","INJURIES")])
+officialnames <-tolower(
+                c( "Astronomical Low Tide"   ,"Avalanche"         ,
+                   "Blizzard"                ,"Coastal Flood"     ,
+                   "Cold/Wind Chill"         ,"Debris Flow"       ,
+                   "Dense Fog"               ,"Dense Smoke"       ,
+                   "Drought"                 ,"Dust Devil"        ,
+                   "Dust Storm"              ,"Excessive Heat"    ,
+                   "Extreme Cold/Wind Chill" ,"Flash Flood"       ,
+                   "Flood"                   ,"Frost/Freeze"      , 
+                   "Funnel Cloud"            ,"Freezing Fog"      ,
+                   "Hail"                    ,"Heat"              , 
+                   "Heavy Rain"              ,"Heavy Snow"        ,
+                   "High Surf"               ,"High Wind"         ,
+                   "Hurricane (Typhoon)"     ,"Ice Storm"         ,
+                   "Lake-Effect Snow"        ,"Lakeshore Flood"   ,
+                   "Lightning"               ,"Marine Hail"       ,
+                   "Marine High Wind"        ,"Marine Strong Wind",
+                   "Marine Thunderstorm Wind","Rip Current"       ,
+                   "Seiche"                  ,"Sleet"             ,
+                   "Storm Surge/Tide"        ,"Strong Wind"       ,
+                   "Thunderstorm Wind"       ,"Tornado"           ,
+                   "Tropical Depression"     ,"Tropical Storm"    , 
+                   "Tsunami"                 ,"Volcanic Ash"      ,
+                   "Waterspout"              ,"Wildfire"          ,
+                   "Winter Storm"            ,"Winter Weather"
+    ))
+##-----------------------------------------------------------------------
+##-----------------------------------------------------------------------
+library(stringdist)
+amatch(x = weather.data$EVTYPE, table = official.events, maxDist = 5)
+for(i in c(0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9 ,10)){
+    matchedlist <- amatch(x = NOAASub$EVTYPE,useBytes = T,
+                          table = officialnames, maxDist = i)
+    print(paste(i ,"   ",length(unique(matchedlist)),"   ",sum(is.na(matchedlist))))
+    
+}
+
+amatch(c("Winter Weather","Winter Storm","Waterspout","Frost Freeze" ),
+       table =("Weather") ,
+       maxDist = 7)
+
+
+
+##-----------------------------------------------------------------------
+##-----------------------------------------------------------------------
+
+
+
+##-----------------------------------------------------------------------
+##-----------------------------------------------------------------------
 NOAASub$HarmfulEconomic <-rowSums(NOAASub[,c("CROPDMG","PROPDMG")])
-#NOAASub <- subset(NOAASub,select = c(EVTYPE,HarmfulHealth,HarmfulEconomic))
+
+Fatalities <-sum(NOAASub$FATALITIES)
+paste("Total Fatalities near",round(Fatalities/10**3,2)," thousand citizen.")
+Injuries <- sum(NOAASub$INJURIES)
+paste("Total Injuries near",round(Injuries/10**3,2)," thousand citizen.")
+PropDmg <- sum(NOAASub$PROPDMG)
+paste("Total properties damage cost around",round(PropDmg/10**12,2)," billion US dollar.")
+CropDmg <- sum(NOAASub$CROPDMG)
+paste("Total crops damage cost around",round(CropDmg/10**12,2)," billion US dollar.")
 ##-----------------------------------------------------------------------
 ##-----------------------------------------------------------------------
-TopHealthHarm <- arrange(NOAASub,HarmfulHealth) %>%
-                 subset(.,select = c(EVTYPE,HarmfulHealth))%>% `rownames<-`(.,NULL) %>%
+TopFATALITIES <- subset(NOAASub,select = c(EVTYPE,FATALITIES))%>% 
                  top_n(.,10) 
 #by thousands
-g<-ggplot(TopHealthHarm , aes(x=EVTYPE, y= HarmfulHealth/10**3)) 
+g<-ggplot(TopFATALITIES, aes(x=reorder(EVTYPE,FATALITIES), y= FATALITIES/10**3)) 
 (g + geom_bar(stat="identity",width=0.5, color="black", fill="red4")
    + coord_flip()
-   + labs(title = "Events most harmful with respect to population health") 
-   + labs(x = "Event Type", y = "FATALITIES and INJURIES (by thousands)")
+   + labs(title = "Harmful Events with respect to Fatalities") 
+   + labs(x = "Event", y = "Fatalities (by thousands)")
    + theme_minimal()  )
 
-toph <- TopHealthHarm[which.max(TopHealthHarm$HarmfulHealth),]
-paste(toph[[1]],round(toph[2]/10**3,4),"By thousands")
+toph <- TopFATALITIES[which.max(TopFATALITIES$FATALITIES),]
+paste("The Most FATALITIES caused by",toph[[1]],"near",round(toph[2]/10**3,4),"thousand citizen")
+
 ##-----------------------------------------------------------------------
 ##-----------------------------------------------------------------------
-TopEconHarm <- arrange(NOAASub,HarmfulEconomic) %>%
-               subset(.,select = c(EVTYPE,HarmfulEconomic)) %>%
+TopINJURIES <- subset(NOAASub,select = c(EVTYPE,INJURIES))  %>% 
+               top_n(.,10) 
+#by thousands
+g<-ggplot(TopINJURIES , aes(x=reorder(EVTYPE,INJURIES), y= INJURIES/10**3)) 
+(g + geom_bar(stat="identity",width=0.5, color="black", fill="firebrick3")
+    + coord_flip()
+    + labs(title = "Harmful Events with respect to Injuries") 
+    + labs(x = "Event", y = " Injuries (by thousands)")
+    + theme_minimal()  )
+
+toph <- TopINJURIES[which.max(TopINJURIES$INJURIES),]
+paste("The Most INJURIES caused by",toph[[1]],"near",round(toph[2]/10**3,4),"thousand citizen")
+##-----------------------------------------------------------------------
+##-----------------------------------------------------------------------
+TopEconHarm <- subset(NOAASub,select = c(EVTYPE,HarmfulEconomic))%>% 
                top_n(.,10)
 
-p<-ggplot(TopEconHarm , aes(x=as.factor(EVTYPE), y= (HarmfulEconomic/10**12))) 
+p<-ggplot(TopEconHarm , aes(x=reorder(EVTYPE,HarmfulEconomic), y= (HarmfulEconomic/10**12))) 
 (p + geom_bar(stat="identity",width=0.5,color="black", fill="blue3")
    + coord_flip()
-   + labs(title = "Events have the greatest economic consequences ") 
-   + labs(x = "Event Type", y = "economic cost by billions US dollar")
+   + labs(title = "Events have the greatest economic Cost ") 
+   + labs(x = "Event", y = "Cost by billions US dollar")
    + theme_minimal()  )
 toph <- TopEconHarm[which.max(TopEconHarm$HarmfulEconomic),]
-paste(toph[[1]],round(toph[2]/10**12,4),"By billions")
+paste("The Most economic damage caused by",toph[[1]],"around",round(toph[2]/10**12,4),"billion US Dollar")
 ##-----------------------------------------------------------------------
